a4be3ea6c69969e7fd94934d1a43bcff95b8fedf
Romulo Galiao <Romulo.Galiao@googlemail.com>
2021-04-23 00:09:08 +0200

    profil icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,34 @@
 import './header.scss'
-import { FC, useEffect, useState } from "react";
+import { FC, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 
 
-
 export const Header: FC<{ signedIn?: boolean }> = ({ signedIn }) => {
+  const [open, setOpen] = useState(false);
 
-  function handleLogBtn(){
-    console.log("hello log");
-  }
+  const onClick = () => setOpen(!open);
+  
+  const handleClick = (e) => {
+    console.log(e.target);
+    const isliOutside = !e.target.closest("li");
+    const isaOutside = !e.target.closest("a");
+    
+    if ((isaOutside|| isliOutside) && open) {
+      setOpen(false);
+    }
+  };
 
-  function handlePlusBtn() {
-console.log("hello plus");  }
+  useEffect(() => {
+    
+    document.addEventListener("click", handleClick);
+    
+    return () => {
+    
+      document.removeEventListener("click", handleClick);
+    
+    };
+  }, [open]);
 
   return (
     <header>
@@ -31,29 +47,36 @@ console.log("hello plus");  }
             <Link className="link-res" to="/extras"> <i className="material-icons">note_add</i></Link>
            
            <div className="myCircle ">
-           <i className="material-icons circle" onClick={handlePlusBtn}>add_circle</i>
+           <i className="material-icons circle">add_circle</i>
+           
            </div>
             </div>
             
             
-            <Link className="profil-link " to="/profile">
-             <i onClick={handleLogBtn} className="material-icons">person</i>
-             {/* <i onClick={handleLogBtn} className="material-icons">logout</i>
-             <i onClick={handleLogBtn} className="material-icons">login</i> */}
+            <Link   className="profil-link " to="/profile">
+             <i onClick={onClick} className="material-icons">person</i>
             </Link>
+
+            <div  
+             className={`menu ${open ? "active" : "inactive"}`}
+            >
+                   <li><a className="log" >logOut</a></li> 
+                   {/* href="/auth/login" */}
+            </div>
           
           </>
         )}
       </nav>
+
       {signedIn ? (
         <>
-        
         </>
       ) : (
         <>
           <Link to="/auth/login">Login</Link>
         </>
       )}
+
     </header>
   );
 };
