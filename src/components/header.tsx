f2e00d360cef839906a9f76434a405c9f9ff5cdc
Romulo Galiao <Romulo.Galiao@googlemail.com>
2021-04-27 15:21:11 +0200

    ui color &  icon size

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,29 +3,23 @@ import { FC, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 
-
 export const Header: FC<{ signedIn?: boolean }> = ({ signedIn }) => {
-
   const [open, setOpen] = useState(false);
   
-  const onClick = () => setOpen(!open);
-  
-  const handleClick = (e) => {
+  const HandleClick = (e) => {
     const isliOutside = !e.target.closest("li");
     const isaOutside = !e.target.closest("a");
-    
+
     if ((isaOutside|| isliOutside) && open) {
     setOpen(false);  
+    }
   };
 
   useEffect(() => {
-    
-    document.addEventListener("click", handleClick);
-    
+    document.addEventListener("click", HandleClick);
+
     return () => {
-    
-      document.removeEventListener("click", handleClick);
-    
+      document.removeEventListener("click", HandleClick);
     };
   }, [open]);
 
@@ -36,24 +30,22 @@ export const Header: FC<{ signedIn?: boolean }> = ({ signedIn }) => {
           <>
           <h2>hello</h2>
           <div className="nav-bar">
-            <Link className="link" to="/">Menus </Link> 
-            <Link className="link-res" to="/"> <i className="material-icons">menu</i></Link> 
-           
-            <Link className="link" to="/items"> Food/Drinks</Link>
-            <Link className="link-res" to="/items"> <i className="material-icons">restaurant_menu</i></Link>
+              <Link className="link" to="/">Menus </Link> 
+              <Link className="link-res" to="/"> <i className="material-icons">menu</i></Link> 
             
-            <Link className="link" to="/extras"> Extras/Toppings</Link>
-            <Link className="link-res" to="/extras"> <i className="material-icons">note_add</i></Link>
-           
-           <div className="myCircle ">
-           <i className="material-icons circle">add_circle</i>
-           
-           </div>
-            </div>
+              <Link className="link" to="/items"> Food/Drinks</Link>
+              <Link className="link-res" to="/items"> <i className="material-icons">restaurant_menu</i></Link>
+              
+              <Link className="link" to="/extras"> Extras/Toppings</Link>
+              <Link className="link-res" to="/extras"> <i className="material-icons">note_add</i></Link>
             
+              <div className="myCircle ">
+                <i className="material-icons circle">add_circle</i>
+              </div>
+          </div>
             
             <Link   className="profil-link " to="/profile">
-             <i onClick={onClick} className="material-icons">person</i>
+             <i onClick={()=> setOpen(!open)} className="material-icons">person</i>
             </Link>
 
             <div  
